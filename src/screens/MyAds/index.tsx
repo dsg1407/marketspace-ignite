ee514efcf6d5171e4ed370a51c6bf6b2c2e3939c
Daniel S Goncalves <dsg1407@gmail.com>
2024-05-27 10:12:37 -0300

    Test Routes MyAds

--- src/screens/MyAds/index.tsx
+++ src/screens/MyAds/index.tsx
@@ -1,9 +1,19 @@
 import { Container, Title } from './styles'
+import { Button } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+import { AppNavigatorRoutesProps } from '@/routes/app.routes'
 
 export function MyAds() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
   return (
     <Container>
       <Title>MyAds</Title>
+      <Button
+        title="myAdDetails"
+        onPress={() => navigation.navigate('adDetails')}
+      />
     </Container>
   )
 }
