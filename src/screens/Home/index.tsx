dcf161a9e524db4f7243c8e3b8fc25052ff32744
Daniel S Goncalves <dsg1407@gmail.com>
2024-05-27 10:12:58 -0300

    Starts Home screen layout

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,60 @@
-import { Container, Title } from './styles'
+import {
+  ArrowIcon,
+  Container,
+  Frame,
+  FrameBackground,
+  FrameText,
+  FrameView,
+  Label,
+  SignText,
+  TagIcon,
+} from './styles'
+import { useNavigation } from '@react-navigation/native'
+
+import { AppNavigatorRoutesProps } from '@/routes/app.routes'
+
+import { HomeHeader } from '@/components/HomeHeader'
+import { Input } from '@/components/Input'
+import { Card } from '@/components/Card'
 
 export function Home() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleMyAds() {
+    navigation.navigate('myAds')
+  }
+
+  function handleFilter() {
+    console.log('filtro')
+  }
   return (
     <Container>
-      <Title>Home</Title>
+      <HomeHeader />
+      <Label>Seus produtos anunciados para venda</Label>
+      <Frame onPress={handleMyAds}>
+        <FrameBackground />
+        <FrameView>
+          <TagIcon />
+          <FrameText bold>
+            4 {'\n'}
+            <FrameText>anúncios ativos</FrameText>
+          </FrameText>
+          <SignText>Meus anúncios</SignText>
+          <ArrowIcon />
+        </FrameView>
+      </Frame>
+
+      <Label>Compre produtos variados</Label>
+      <Input
+        type="search"
+        placeholder="Buscar anúncio"
+        showFilter={handleFilter}
+      />
+      <Card />
     </Container>
   )
 }
+
+//TODO - Componentes Cards
+//TODO - Flatlist
+//TODO - Filtro
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,15 +1,72 @@
 import styled, { css } from 'styled-components/native'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { ArrowRight, Tag } from 'phosphor-react-native'
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
-  align-items: center;
+  padding: 20px 24px;
+`
+
+export const Label = styled.Text`
+  margin-top: 32px;
+  margin-bottom: 12px;
+  ${({ theme }) => css`
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.S}px;
+    color: ${theme.COLORS.BASE.GRAY_3};
+  `}
+`
+
+export const Frame = styled.TouchableOpacity`
+  width: 100%;
+  min-height: 66px;
+  max-height: 66px;
   justify-content: center;
 `
 
-export const Title = styled.Text`
+export const FrameBackground = styled.View`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background-color: ${({ theme }) => theme.COLORS.PRODUCT.BLUE_LIGHT};
+  opacity: 0.1;
+  border-radius: 6px;
+`
+
+export const FrameView = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  padding: 12px 20px 12px 16px;
+`
+
+export const FrameText = styled(Text)<{ bold?: boolean }>`
+  ${({ theme, bold }) => css`
+    font-size: ${bold ? theme.FONT_SIZE.L : theme.FONT_SIZE.S}px;
+    font-family: ${bold ? theme.FONT_FAMILY.BOLD : theme.FONT_FAMILY.REGULAR};
+    color: ${theme.COLORS.BASE.GRAY_2};
+  `}
+  flex: 1;
+`
+export const SignText = styled.Text`
   ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.XS}px;
     font-family: ${theme.FONT_FAMILY.BOLD};
-    font-size: ${theme.FONT_SIZE.XL}px;
     color: ${theme.COLORS.PRODUCT.BLUE};
   `}
+`
+
+export const ArrowIcon = styled(ArrowRight).attrs(({ theme }) => ({
+  color: theme.COLORS.PRODUCT.BLUE,
+  size: 16,
+}))`
+  margin-left: 8px;
+`
+
+export const TagIcon = styled(Tag).attrs(({ theme }) => ({
+  color: theme.COLORS.PRODUCT.BLUE,
+  size: 22,
+}))`
+  margin-right: 16px;
 `
